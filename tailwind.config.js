92f34eb2071a72a391fae9c66542b2312a3bd225
Ajay Jakate <ajay.jakate@gmail.com>
2022-04-23 13:09:04 -0500

    WIP, base site. need to upgrade tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'basha-turq': '#2dc3dd',
+                'basha-yellow': '#fffd72',
+                'basha-orange': '#f6a870',
+                'basha-bold-orange': '#ff8732'
+            },
+        }
+    }
+}
