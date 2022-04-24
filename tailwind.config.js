595a37d84b7906459915a0f7db150cc58c8b91a5
Ajay Jakate <ajay.jakate@gmail.com>
2022-04-24 16:16:41 -0500

    widgets done except for numbers

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
                 'basha-turq': '#2dc3dd',
                 'basha-yellow': '#fffd72',
                 'basha-orange': '#f6a870',
-                'basha-bold-orange': '#ff8732'
+                'basha-bold-orange': '#ff8732',
+                'basha-green': '#94e681'
             },
         }
     }
