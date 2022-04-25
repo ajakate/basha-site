13728fa61dedbf8dafada310137b6db37b95dfd4
ajakate <ajakate@users.noreply.github.com>
2022-04-25 00:36:40 +0000

    deploy: cdb7c4145351ebadf08c937e34106ffbfedb7e85

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+$(document).ready(function() {
+    $("#hamburger").click(function() {
+        $("#hamburger").addClass("hidden");
+        $("#exit").removeClass("hidden");
+        $("#menu").removeClass("hidden");
+    });
+
+    $("#exit").click(function() {
+        $("#exit").addClass("hidden");
+        $("#hamburger").removeClass("hidden");
+        $("#menu").addClass("hidden");
+    });
+});
