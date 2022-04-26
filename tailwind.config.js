4ddaebc1287b87380bf4598b884cc03bc3866052
Ajay Jakate <ajay.jakate@gmail.com>
2022-04-26 11:56:14 -0500

    site is responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
                 'basha-bold-orange': '#ff8732',
                 'basha-green': '#94e681'
             },
+            gridRowStart: {
+                '8': '8',
+                '9': '9',
+                '10': '10',
+                '11': '11',
+                '12': '12',
+                '13': '13',
+              }
         }
     }
 }
