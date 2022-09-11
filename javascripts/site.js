598fc61a8973da75a135c34d668e550825f7dcb8
ajakate <ajakate@users.noreply.github.com>
2022-09-11 22:03:59 +0000

    deploy: d88663bc2902b969f1015815cbad5c6870c07f64

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,3 +1,28 @@
+var tabButtons = [
+    "install",
+    "create",
+    "share",
+    "export",
+    "restore"
+];
+
+function buttonId(str) {
+    return "#" + str + "_button";
+};
+
+function contentId(str) {
+    return "#" + str + "_content";
+};
+
+function setTab(elemId) {
+    tabButtons.forEach(function(id) {
+        $(buttonId(id)).addClass("opacity-80");
+        $(contentId(id)).addClass("hidden");
+    });
+    $(buttonId(elemId)).removeClass("opacity-80");
+    $(contentId(elemId)).removeClass("hidden");
+};
+
 $(document).ready(function() {
     $("#hamburger").click(function() {
         $("#hamburger").addClass("hidden");
@@ -10,4 +35,25 @@ $(document).ready(function() {
         $("#hamburger").removeClass("hidden");
         $("#menu").addClass("hidden");
     });
+
+    addEventListener('hashchange', (_) => {
+        var hash = window.location.hash.substring(1);
+
+        if (tabButtons.includes(hash)) {
+            setTab(hash);
+        };
+    });
+
+    tabButtons.forEach(function(elemId) {
+        $(buttonId(elemId)).click(function() {
+            setTab(elemId);
+            window.location.hash = elemId;
+        });
+    });
+
+    var hash = window.location.hash.substring(1);
+
+    if (tabButtons.includes(hash)) {
+        setTab(hash);
+    };
 });
